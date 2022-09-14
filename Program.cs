a1b2a8ee102fd1c55bfd3d22a2e3e14e53f056f0
hddisk <109894765+hddisk@users.noreply.github.com>
2022-09-14 17:11:20 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+string[] array = new string[]{"hello","2","world","(-:"};
+//Console.WriteLine(array[0] + array[1] + array[2] + array[3]);
+int count = 0;
+for (int i=0;i<array.Length;i++)
+{
+//Console.WriteLine(array[i]);
+//string text = array[i];
+if(array[i].Length<4)
+    {
+        count += 1;
+    }
+}
+//Console.WriteLine(count);
+string[] array2 = new string[count];
+
+count = 0;
+for (int i=0;i<array.Length;i++)
+{
+    if(array[i].Length<4)
+    {
+        array2[count] = array[i];
+        Console.WriteLine(array2[count]);
+        count += 1;
+    }
+}
